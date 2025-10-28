565e973b41a8c14dd4bda77c5e58c7ef99d57eb2
Ana Paula <210077816+mouraoli86@users.noreply.github.com>
2025-10-27 21:06:04 -0300

    imprimindo_carta1.Carta2.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,17 +7,59 @@
 int main() {
 
   printf("Desafio Super Trunfo - Países.\n");
+  
  // Área para definição das variáveis para armazenar as propriedades das cidades
- Estado char 'Ceará'
- codigo_da_carta char 'A01'
- nome_da_cidade 'Fortaleza'
- populacao int 2.000.000;
- area_em_km² float 10.000.00;
- PIB float 6.33;
- numero_de_pntos_turisticos int 23;
+ // Carta 1
+ char Estado1;
+char codigo_da_carta1[0];
+char nome_da_cidade1[0];
+int populacao1, ponto_turistico1;
+float area1, pib1;
 
-
+  //carta2
+  char estado2;
+  char codigo_da_carta2[0];
+  char nome_da_cidade2[0];
+  int polulacao2, ponto_turistico2;
+  float area2, pib2;
+  
   // Área para entrada de dados
+// Leitura - Carta 1
+  printf("cadastro carta 1:\n");
+  printf("Estado de A-H : ");
+  scanf("%c", &estado1);
+  printf("Código da carta: (ex: A01) ");
+  scanf("%c", &codigo_da_carta1);
+  printf("Nome da cidade: ");
+  scanf("%c", &nome_da_cidade1);
+  printf("População: ");
+  scanf("%d", &populacao1);
+  printf("Ponto turístico: ");
+  scanf("%d", &ponto_turistico1);
+  printf(" Área (em KM²): ");
+  scanf("%f", &area1);
+  printf("PIB (em bilhões): ");
+  scanf("%f", &pib1);
+  
+//Leitura - Carta 2
+
+  printf("cadastro carta 2:\n");
+  printf("Estado de A-H : ");
+  scanf("%c", &estado2);
+  printf("Código da carta: (ex: A02) ");
+  scanf("%c", &codigo_da_carta2);
+  printf("Nome da cidade: ");
+  scanf("%c", &nome_da_cidade2);
+  printf("População: ");
+  scanf("%d", &populacao2);
+  printf("Ponto turístico: ");
+  scanf("%d", &ponto_turistico2);
+  printf(" Área (em KM²): ");
+  scanf("%f", &area2);
+  printf("PIB (em bilhões): ");
+  scanf("%f", &pib2);
+  
+  
 
   // Área para exibição dos dados da cidade
 
